b05819446be81eb6c3e74fceaf1d03e82f3dbc61
Kevin Kipp <kevin.kipp@gmail.com>
2018-12-05 22:50:29 -0600

    Rename for clarity

--- 01/part2.hs
+++ 01/part2.hs
@@ -5,14 +5,14 @@ readInt (x:xs)
     | x == '+' = read xs
     | x == '-' = read ('-':xs)
 
-findFirstFreq :: [Int] -> Int
-findFirstFreq xs = findFirstFreq' xs (Set.empty, 0, Nothing)
+findFirstFreqRepeat :: [Int] -> Int
+findFirstFreqRepeat xs = findFirstFreqRepeat' xs (Set.empty, 0, Nothing)
 
-findFirstFreq' :: [Int] -> (Set.Set Int, Int, Maybe Int) -> Int
-findFirstFreq' xs (set, freq, Just x) = x
-findFirstFreq' xs (set, freq, Nothing) =
+findFirstFreqRepeat' :: [Int] -> (Set.Set Int, Int, Maybe Int) -> Int
+findFirstFreqRepeat' xs (set, freq, Just x) = x
+findFirstFreqRepeat' xs (set, freq, Nothing) =
     let result = foldl processFreq (set, freq, Nothing) xs
-    in findFirstFreq' xs result
+    in findFirstFreqRepeat' xs result
 
 processFreq :: (Set.Set Int, Int, Maybe Int) -> Int -> (Set.Set Int, Int, Maybe Int)
 processFreq (set, freq, Just x) y = (set, freq, Just x)
@@ -22,6 +22,6 @@ processFreq (set, freq, Nothing) x =
 
 main = do
     input <- readFile "./Input.txt"
-    let answer = findFirstFreq . map readInt . lines $ input
+    let answer = findFirstFreqRepeat . map readInt . lines $ input
     print answer
 
