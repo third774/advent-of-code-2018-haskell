d47ff4e5d352734277f7a85a593e1163cd27eb9c
Kevin Kipp <kevin.kipp@gmail.com>
2018-12-06 09:22:27 -0600

    Format using hie

--- 01/part2.hs
+++ 01/part2.hs
@@ -1,9 +1,8 @@
-import qualified Data.Set as Set
+import qualified Data.Set                      as Set
 
 readInt :: String -> Int
-readInt (x:xs)
-    | x == '+' = read xs
-    | x == '-' = read ('-':xs)
+readInt (x : xs) | x == '+' = read xs
+                 | x == '-' = read ('-' : xs)
 
 findFirstFreqRepeat :: [Int] -> Int
 findFirstFreqRepeat xs = findFirstFreqRepeat' xs (Set.empty, 0, Nothing)
@@ -12,13 +11,15 @@ findFirstFreqRepeat' :: [Int] -> (Set.Set Int, Int, Maybe Int) -> Int
 findFirstFreqRepeat' xs (set, freq, Just x) = x
 findFirstFreqRepeat' xs (set, freq, Nothing) =
     let result = foldl processFreq (set, freq, Nothing) xs
-    in findFirstFreqRepeat' xs result
+    in  findFirstFreqRepeat' xs result
 
-processFreq :: (Set.Set Int, Int, Maybe Int) -> Int -> (Set.Set Int, Int, Maybe Int)
-processFreq (set, freq, Just x) y = (set, freq, Just x)
-processFreq (set, freq, Nothing) x = 
-    if Set.member newFreq set then (set, freq, Just newFreq) else (Set.insert newFreq set, newFreq, Nothing)
-        where newFreq = freq + x
+processFreq
+    :: (Set.Set Int, Int, Maybe Int) -> Int -> (Set.Set Int, Int, Maybe Int)
+processFreq (set, freq, Just x ) y = (set, freq, Just x)
+processFreq (set, freq, Nothing) x = if Set.member newFreq set
+    then (set, freq, Just newFreq)
+    else (Set.insert newFreq set, newFreq, Nothing)
+    where newFreq = freq + x
 
 main = do
     input <- readFile "./Input.txt"
