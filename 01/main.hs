4ab31461919384efb656071a389cc2f183695292
Kevin Kipp <kevin.kipp@gmail.com>
2018-12-05 21:22:20 -0600

    Add first solution

---
+++ 01/main.hs
@@ -0,0 +1,10 @@
+
+readInt :: String -> Int
+readInt (x:xs)
+    | x == '+' = read xs
+    | x == '-' = read ('-':xs)
+
+main = do
+    input <- readFile "./Input.txt"
+    let answer = sum $ map readInt $ lines input
+    print answer
